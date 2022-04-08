cc755529fc1e65d223656902e0c51f1406715c6b
Leandro Sales <leandrossales.96@gmail.com>
2022-04-08 12:25:18 -0300

    add transaction form

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ByteBankApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(
+        body: TransactionForm(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class TransactionForm extends StatelessWidget {
+  const TransactionForm({Key? key}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: const ListTransaction());
+      appBar: AppBar(
+        title: const Text('ByteBank'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              style: TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              style: TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0,00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Confirmar'))
+        ],
+      ),
+    );
   }
 }
 
@@ -43,12 +65,21 @@ class ListTransaction extends StatelessWidget {
   const ListTransaction({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransactionItem(Transaction(value: 500, accountNumber: 12546)),
-        TransactionItem(Transaction(value: 248, accountNumber: 12946)),
-        TransactionItem(Transaction(value: 6987, accountNumber: 12366)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          TransactionItem(Transaction(value: 500, accountNumber: 12546)),
+          TransactionItem(Transaction(value: 248, accountNumber: 12946)),
+          TransactionItem(Transaction(value: 6987, accountNumber: 12366)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
