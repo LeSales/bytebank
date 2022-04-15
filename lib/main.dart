4571bf9544fb0ef3cfab54575f50853fcbe657ae
Leandro Sales <leandrossales.96@gmail.com>
2022-04-15 10:50:38 -0300

    refact form fields

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class ByteBankApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: TransactionForm(),
       ),
     );
@@ -21,7 +21,11 @@ class ByteBankApp extends StatelessWidget {
 }
 
 class TransactionForm extends StatelessWidget {
-  const TransactionForm({Key? key}) : super(key: key);
+  TransactionForm({Key? key}) : super(key: key);
+
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+  final TextEditingController _valueNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,31 +35,63 @@ class TransactionForm extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: TextField(
-              style: TextStyle(fontSize: 24),
-              decoration: InputDecoration(
-                labelText: 'Número da conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+            controller: _accountNumberController,
+            hintText: '0000',
+            labelText: 'Número da Conta',
           ),
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: TextField(
-              style: TextStyle(fontSize: 24),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0,00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+            controller: _valueNumberController,
+            hintText: '0,00',
+            labelText: 'Valor',
+            icon: Icons.monetization_on,
           ),
-          ElevatedButton(onPressed: () {}, child: const Text('Confirmar'))
+          ElevatedButton(
+            child: const Text('Confirmar'),
+            onPressed: () {
+              final int? accountNumber =
+                  int.tryParse(_accountNumberController.text);
+              final double? value =
+                  double.tryParse(_valueNumberController.text);
+              if (![accountNumber, value].contains(null)) {
+                final createdTransaction =
+                    Transaction(value: value!, accountNumber: accountNumber!);
+              }
+            },
+          )
         ],
+      ),
+    );
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String hintText;
+  final IconData? icon;
+
+  const Editor({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    required this.hintText,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        controller: controller,
+        style: const TextStyle(fontSize: 24),
+        decoration: InputDecoration(
+          icon: icon != null ? Icon(icon) : null,
+          labelText: labelText,
+          hintText: hintText,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
